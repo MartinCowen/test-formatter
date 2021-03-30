608273f28b03154b55822df4c82bd79212fa6417
Martin Cowen <work@martincowen.me.uk>
2021-03-30 14:39:06 +0100

    First commit. Based on SourceFormatForWeb as a visual design. Converts MSTest test method format C++ to minunit C format.

---
+++ TestFormatter/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.cmbFrom = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnCopy = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbTo = New System.Windows.Forms.ComboBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.SplitContainer1)
+        Me.Panel1.Location = New System.Drawing.Point(2, 67)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(925, 571)
+        Me.Panel1.TabIndex = 0
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txtInput)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtOutput)
+        Me.SplitContainer1.Size = New System.Drawing.Size(925, 571)
+        Me.SplitContainer1.SplitterDistance = 461
+        Me.SplitContainer1.SplitterWidth = 5
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'txtInput
+        '
+        Me.txtInput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtInput.Font = New System.Drawing.Font("Lucida Console", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtInput.Location = New System.Drawing.Point(0, 0)
+        Me.txtInput.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtInput.Multiline = True
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtInput.Size = New System.Drawing.Size(461, 571)
+        Me.txtInput.TabIndex = 0
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtOutput.Font = New System.Drawing.Font("Lucida Console", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtOutput.Location = New System.Drawing.Point(0, 0)
+        Me.txtOutput.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtOutput.Size = New System.Drawing.Size(459, 571)
+        Me.txtOutput.TabIndex = 0
+        '
+        'cmbFrom
+        '
+        Me.cmbFrom.FormattingEnabled = True
+        Me.cmbFrom.Location = New System.Drawing.Point(58, 24)
+        Me.cmbFrom.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.cmbFrom.Name = "cmbFrom"
+        Me.cmbFrom.Size = New System.Drawing.Size(174, 23)
+        Me.cmbFrom.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 28)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "From"
+        '
+        'btnCopy
+        '
+        Me.btnCopy.Location = New System.Drawing.Point(796, 24)
+        Me.btnCopy.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.btnCopy.Name = "btnCopy"
+        Me.btnCopy.Size = New System.Drawing.Size(128, 33)
+        Me.btnCopy.TabIndex = 4
+        Me.btnCopy.Text = "Copy"
+        Me.btnCopy.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(419, 24)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(88, 33)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(555, 28)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(19, 15)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "To"
+        '
+        'cmbTo
+        '
+        Me.cmbTo.FormattingEnabled = True
+        Me.cmbTo.Location = New System.Drawing.Point(582, 25)
+        Me.cmbTo.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.cmbTo.Name = "cmbTo"
+        Me.cmbTo.Size = New System.Drawing.Size(174, 23)
+        Me.cmbTo.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(933, 640)
+        Me.Controls.Add(Me.cmbTo)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCopy)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbFrom)
+        Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "Form1"
+        Me.Text = "Test Formatter"
+        Me.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents cmbFrom As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnCopy As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmbTo As ComboBox
+End Class
---
+++ TestFormatter/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Net
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PopulateLangs()
+    End Sub
+    Private Sub PopulateLangs()
+        With cmbFrom
+            .Enabled = False
+            .Items.Add("MSTest")
+            .Enabled = True
+        End With
+
+        With cmbTo
+            .Enabled = False
+            .Items.Add("minunit")
+            .Enabled = True
+        End With
+
+        cmbFrom.SelectedIndex = 0
+        cmbTo.SelectedIndex = 0
+
+    End Sub
+
+
+    Private Sub txtInput_TextChanged(sender As Object, e As EventArgs) Handles txtInput.TextChanged
+        StartSourceFormat()
+    End Sub
+
+    Private Sub cmbLangs_TextChanged(sender As Object, e As EventArgs) Handles cmbFrom.TextChanged
+        StartSourceFormat()
+    End Sub
+
+    Private Sub btnCopy_Click(sender As Object, e As EventArgs) Handles btnCopy.Click
+        My.Computer.Clipboard.SetText(txtOutput.Text)
+    End Sub
+    Private Sub StartSourceFormat()
+        Dim nl As String = System.Environment.NewLine
+        txtOutput.Text = ""
+        For Each ln As String In txtInput.Text.Split(nl)
+            txtOutput.Text &= LineFormatMSTestToMinunit(ln)
+        Next ln
+    End Sub
+    Private Function LineFormatMSTestToMinunit(ln As String) As String
+        Dim nl As String = System.Environment.NewLine
+        Dim r As String = ""
+        Static test_name As String = "" 'test name needs to be retained between lines
+
+        If ln.Contains("TEST_METHOD") Then
+            test_name = Trim(ln.Split("(")(1).Split(")")(0))
+            r &= "static char* mu_test" & test_name & "(void)" & nl
+        ElseIf ln.Contains("Assert::AreEqual") Then
+            Dim s As String
+            'remove the uint32_t casts
+            s = ln.Replace("(uint32_t)", "")
+            'replace the comma with equal test
+            s = s.Replace(",", " ==")
+            'create the new assert
+            s = s.Replace("Assert::AreEqual(", "mu_assert(""" & test_name & " failed" & """, ")
+            r &= s & nl
+        Else
+            r &= ln & nl
+        End If
+        Return r
+    End Function
+
+
+    ''' <summary>
+    ''' Counts instances of substrings within strings
+    ''' </summary>
+    ''' <param name="s">string to be searched</param>
+    ''' <param name="substr">substring</param>
+    ''' <returns>Number of instances</returns>
+    Public Function CountInstancesOfSubstrings(s As String, substr As String) As Integer
+        Dim c As Integer = 0
+        For i As Integer = 0 To s.Length - substr.Length
+            If s.Substring(i, substr.Length) = substr Then
+                c += 1
+            End If
+        Next i
+        Return c
+    End Function
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtInput.Text = String.Empty
+        txtInput.Focus()
+    End Sub
+
+End Class
